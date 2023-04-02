05064bc41e5464e54b2b7be97c5c34941b1a98e7
KIMSEI1124 <workju1124@gmail.com>
2023-04-02 21:19:52 +0900

    feat: 장바구니 Json 파일 수정
    - 서버에서 넘어오는 Json 포멧이 변경이 되어 해당 포멧으로 변경하여 총 가격을 출력하는 기능을
    구현하였다.

--- front-end/src/components/Cart/Payment/index.jsx
+++ front-end/src/components/Cart/Payment/index.jsx
@@ -1,37 +1,33 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React from "react";
 
 import * as S from "./index.styled";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { faEquals } from "@fortawesome/free-solid-svg-icons";
 
-function Payment({ data }) {
-  const [price, setPrice] = useState(0);
+function Payment({ price, courierFee }) {
+  const courierPrice = 3000;
+  const formatPrice = Number(price).toLocaleString();
+  let totalFormatPrice = price;
 
-  const getPrice = useCallback(async () => {
-    data.map((product) => {
-      if (product.checked) {
-        return setPrice((price) => price + product.productPrice * product.quantity);
-      }
-      return setPrice((price) => price);
-    });
-  }, [data]);
-
-  useEffect(() => {
-    getPrice();
-  }, [getPrice, data]);
+  if (courierFee) {
+    totalFormatPrice = Number(price + courierPrice).toLocaleString();
+  } else {
+    totalFormatPrice = Number(price).toLocaleString();
+  }
 
   return (
     <S.Container>
       <S.Text>
         결제 금액 &nbsp;
-        <S.Price>{price}</S.Price> 원 &nbsp;
+        <S.Price>{formatPrice}</S.Price> 원 &nbsp;
         <FontAwesomeIcon icon={faPlus} />
         &nbsp; 총 배송비 &nbsp;
-        <S.Price>3000</S.Price> 원 &nbsp;
+        {courierFee ? <S.Price>3,000</S.Price> : <S.Price>0</S.Price>}
+        &nbsp;원 &nbsp;
         <FontAwesomeIcon icon={faEquals} />
         &nbsp; 총 주문 금액 &nbsp;
-        <S.Price>{price + 3000}</S.Price> 원
+        <S.Price>{totalFormatPrice}</S.Price> 원
       </S.Text>
     </S.Container>
   );
