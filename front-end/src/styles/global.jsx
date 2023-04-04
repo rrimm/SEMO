cfd3fe7017c2f9ad87250d3e9d3214eaa79ddbc8
KIMSEI1124 <workju1124@gmail.com>
2023-04-05 01:55:30 +0900

    chore: Install emotion reset

--- front-end/src/styles/global.jsx
+++ front-end/src/styles/global.jsx
@@ -1,8 +1,10 @@
 import { Global, css } from "@emotion/react";
+import emotionReset from "emotion-reset";
 
 import { fontStyle } from "./fonts";
 
 const style = css`
+  ${emotionReset}
   ${fontStyle}
 
   * {
